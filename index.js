7a2b06d5bbd8997783b44edf1c534a408a4685f8
deepaksm24 <dpkmdu@gmail.com>
2023-04-16 13:21:30 +0530

    fourth commit

--- index.js
+++ index.js
@@ -48,10 +48,34 @@ app.use(express.static("timeswamp"))
 
 app.get("/", (req, res) => {
 
+
+
+
+
     res.send("Welcome sucessfully connected add /static  e.g http://localhost:9000/static to create timestamp file")
 })
 
 app.get("/static", (req, res) => {
+    
+let time1 = new Date();
+
+let time = utcToIndiantime(time1); 
+
+
+let date = (time.getDate());
+let month = (time.getMonth()) + 1;
+
+
+
+const fname = (`Date-${date}-Month-${month}`);
+
+// console.log(fname);
+
+
+let datestring = time.toUTCString().slice(0, -4);
+
+
+const timeswamp = `last created timeswamp: ${datestring}`
 
     fs.writeFileSync(`${dirpath}/${fname}.txt`, timeswamp, (err) => {
 
